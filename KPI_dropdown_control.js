e12d3a0ff4c6253b6663d8615725cd4cd20c30ea
PreeTiamsawate <preetiamsawate@gmail.com>
2021-12-16 17:13:22 +0700

    Header finished

---
+++ KPI_dropdown_control.js
@@ -0,0 +1,27 @@
+const filterBtn = document.querySelector("#filter-btn");
+const dropdownForm = document.querySelector(".filter-boxes").querySelector(".w3-dropdown-content");
+const cancelFormBtn = document.querySelector("#cancel-form-btn")
+filterBtn.addEventListener('click', function() {
+    if (dropdownForm.className.indexOf("w3-show") == -1) {
+        dropdownForm.className += " w3-show";
+        this.style.border = "2px solid #444444"
+
+    } else {
+        dropdownForm.className = dropdownForm.className.replace(" w3-show", "");
+        this.style.border = "1px solid #DEDEDE"
+
+    }
+})
+cancelFormBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (dropdownForm.className.indexOf("w3-show") == -1) {
+        dropdownForm.className += " w3-show";
+        filterBtn.style.border = "2px solid #444444"
+
+    } else {
+        dropdownForm.className = dropdownForm.className.replace(" w3-show", "");
+        filterBtn.style.border = "1px solid #DEDEDE"
+
+    }
+
+})
