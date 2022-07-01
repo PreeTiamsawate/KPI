1b4453f061773aacce08b34a524d3dc9bac21f5d
unknown <preetiamsawate@gmail.com>
2022-07-01 16:18:07 +0700

    debugging breadcrumb2

--- KPI_breadcrumb_control.js
+++ KPI_breadcrumb_control.js
@@ -6,9 +6,10 @@ if (!sessionStorage.getItem('track')) {
 } else {
     var track = JSON.parse(sessionStorage.getItem('track'));
     for(let page of track){
+        console.log(page.url)
+        console.log(window.location.href)
+        console.log("=================================")
         if(page.url ==  window.location.href ){
-            console.log(page.url)
-            console.log(window.location.href)
             var index = track.indexOf(page);
             track = track.slice(0,index+1)
         }
@@ -19,7 +20,7 @@ for(let page of track){
     breadcrumb.innerHTML += `<li class="breadcrumb-item"><a href=${page.url}>${page.page}</a></li>`
     // breadcrumb.append(list);
 }
-console.log(window.location.href)
+
 console.log(track)
 
 var employeeLinks = document.querySelectorAll('.employee-link');
