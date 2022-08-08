8a17655559bb332d7f7567293b735d76aac264a3
unknown <preetiamsawate@gmail.com>
2022-08-08 10:22:02 +0700

    textarer maxlength 800 to 500

--- KPI_row_status_submit.js
+++ KPI_row_status_submit.js
@@ -1,20 +1,20 @@
-const submitBtnTest = document.querySelector('#submitPage');
+// const submitBtnTest = document.querySelector('#submitPage');
 
-submitBtnTest.addEventListener('click', function(e) {
-    e.preventDefault()
-    const greenRows = document.querySelectorAll("tr[row-status='newly-filled']")
-    const submitBtn = document.querySelector('#submitPage');
-    const paginationWrapper = document.querySelector("#pagination-wrapper")
-    const paginationBtns = document.querySelectorAll("#pagination-wrapper > .btn")
-    for (const greenRow of greenRows) {
-        greenRow.style.backgroundColor = "#FFFFFF";
-        greenRow.setAttribute("row-status", "fully-filled")
+// submitBtnTest.addEventListener('click', function(e) {
+//     e.preventDefault()
+//     const greenRows = document.querySelectorAll("tr[row-status='newly-filled']")
+//     const submitBtn = document.querySelector('#submitPage');
+//     const paginationWrapper = document.querySelector("#pagination-wrapper")
+//     const paginationBtns = document.querySelectorAll("#pagination-wrapper > .btn")
+//     for (const greenRow of greenRows) {
+//         greenRow.style.backgroundColor = "#FFFFFF";
+//         greenRow.setAttribute("row-status", "fully-filled")
 
-    }
-    submitBtn.style.backgroundColor = "#330066";
-    paginationWrapper.style.backgroundColor = "transparent";
-    paginationWrapper.style.cursor = "pointer"
-    for (const paginationBtn of paginationBtns) {
-        paginationBtn.disabled = false
-    }
-})
+//     }
+//     submitBtn.style.backgroundColor = "#330066";
+//     paginationWrapper.style.backgroundColor = "transparent";
+//     paginationWrapper.style.cursor = "pointer"
+//     for (const paginationBtn of paginationBtns) {
+//         paginationBtn.disabled = false
+//     }
+// })
